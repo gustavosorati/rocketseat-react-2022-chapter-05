d7bbe701a9dd63c567606e258affc5e607e57ea0
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-01-31 15:56:38 -0200

    aula 04.03 - Controles nos componentes

--- 04-melhorias-no-storybook/packages/docs/src/stories/Avatar.stories.tsx
+++ 04-melhorias-no-storybook/packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/gustavosorati.png',
     alt: 'Gustavo Sorati',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- 04-melhorias-no-storybook/packages/docs/src/stories/Text.stories.tsx
+++ 04-melhorias-no-storybook/packages/docs/src/stories/Text.stories.tsx
@@ -8,6 +8,29 @@ export default {
   args: {
     children:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, fuga porro quidem dolorem assumenda maiores veritatis dicta illum magnam eligendi corrupti esse perferendis ex, magni vel debitis cupiditate, ratione a.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
