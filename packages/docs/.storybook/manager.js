4130fdb1a03294058ec7267e5abc705b849efb3b
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-01-31 10:23:29 -0200

    aula 05 - Tema dark no Storybook

--- packages/docs/.storybook/manager.js
+++ packages/docs/.storybook/manager.js
@@ -1,6 +1,6 @@
 import {addons} from '@storybook/addons'
-import {theme} from '@storybook/theming'
+import {themes} from '@storybook/theming'
 
 addons.setConfig({
-    theme: theme.dark
+    theme: themes.dark
 })
