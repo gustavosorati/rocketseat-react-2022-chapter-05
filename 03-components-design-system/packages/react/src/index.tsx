9c7d61a5367d493a299500aeea3ea6d7a338366f
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-01-31 14:09:34 -0200

    aula 03.07 - Componente: TextArea

--- 03-components-design-system/packages/react/src/index.tsx
+++ 03-components-design-system/packages/react/src/index.tsx
@@ -1,5 +1,6 @@
 export * from './components/Box'
 export * from './components/Text'
+export * from './components/TextArea'
 export * from './components/Heading'
 export * from './components/Button'
 export * from './components/Avatar'
